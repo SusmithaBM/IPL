4f2f425ac9f0311a7f0eb736e213e1380db750ca
SusmithaBM <86008650+SusmithaBM@users.noreply.github.com>
2023-05-20 18:51:37 +0530

    Add files via upload

---
+++ exp_1/ex1/app-routing.module.ts
@@ -0,0 +1,13 @@
+<div class='container'>
+ <h1>
+
+    welcome to my website
+</h1>
+</div>
+ <div>
+   <a routerlink="homepage">HOME</a>
+   <a routerlink="gallerypage">GALLERY</a>
+   <a routerlink="registerpage">REGISTER</a>
+ </div>
+<router-outlet>
+</router-outlet>
